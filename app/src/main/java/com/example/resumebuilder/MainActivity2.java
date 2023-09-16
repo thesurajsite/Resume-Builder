b91f6bf100c4a1939c0e65334df7f819684662d8
thesurajsite <thesurajsite@gmail.com>
2023-09-16 22:41:06 +0530

    activity 1 front end completed...

--- app/src/main/java/com/example/resumebuilder/MainActivity2.java
+++ app/src/main/java/com/example/resumebuilder/MainActivity2.java
@@ -7,17 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
-    public void openActivity(View v){
-        Toast.makeText(this, "Generating Resume", Toast.LENGTH_SHORT).show();
-        Intent intent=new Intent(this , MainActivity2.class);
-        startActivity(intent);
-    }
 }
